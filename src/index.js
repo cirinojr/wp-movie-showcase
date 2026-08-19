043eb5ab051eafdd0c8507a5eea5a7ee13fd8d00
cirinojr <azauane@gmail.com>
2026-08-19 20:44:23 -0300

    Fix cross-platform editor lint formatting

--- src/index.js
+++ src/index.js
@@ -6,23 +6,23 @@ import { __ } from '@wordpress/i18n';
 import metadata from './block.json';
 
 function Edit() {
-  return (
-    <div { ...useBlockProps() }>
-      <Placeholder
-        label={ __( 'Movie Search', 'wp-movie-showcase' ) }
-        instructions={ __(
-          'The movie search form is rendered on the frontend.',
-          'wp-movie-showcase',
-        ) }
-      />
-    </div>
-  );
+	return (
+		<div {...useBlockProps()}>
+			<Placeholder
+				label={__('Movie Search', 'wp-movie-showcase')}
+				instructions={__(
+					'The movie search form is rendered on the frontend.',
+					'wp-movie-showcase',
+				)}
+			/>
+		</div>
+	);
 }
 
-registerBlockType( metadata.name, {
-  ...metadata,
-  edit: Edit,
-  save() {
-    return null;
-  },
-} );
+registerBlockType(metadata.name, {
+	...metadata,
+	edit: Edit,
+	save() {
+		return null;
+	},
+});
